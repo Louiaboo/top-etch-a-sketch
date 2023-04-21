5d4637ffc00b0cf251ae25a00b57f0e307164cb9
loui <106045306+Louiaboo@users.noreply.github.com>
2023-04-21 15:39:42 +0800

    Add canvas dimension change function

--- etch.js
+++ etch.js
@@ -3,10 +3,24 @@ let sliderValue = document.querySelector(".sliderValue");
 
 slider.oninput = function() {
     sliderValue.innerHTML = `${slider.value} x ${slider.value}`;
+    changeDimensions(slider.value ** 2);
 }
 
+const canvas = document.querySelector(".canvas");
 const defaultSize = 16;
 
 function changeDimensions(size) {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.lastChild);
+    }
+    for (let i = 0; i < size; i++) {
+        let pixel = document.createElement("div");
+        canvas.appendChild(pixel);
+        console.log(i);
+    }
+}
 
+
+window.onload = () => {
+    changeDimensions(defaultSize ** 2);
 }
